826309c4ed641b184989cab40b315fb8bc31d511
MudassarShaykh <161149123+MudassarShaykh@users.noreply.github.com>
2024-09-15 08:09:14 -0700

    Add files via upload

---
+++ Milestone 3/script.js
@@ -0,0 +1,15 @@
+var form = document.querySelector(".form-1");
+var generatedResume = document.querySelector(".generated-resume");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var phone = document.querySelector("#phone").value;
+    var education = document.querySelector("#education").value;
+    var skills = document.querySelector("#skills").value;
+    var experience = document.querySelector("#experience").value;
+    var resumeHtml = "\n<h1 class=\"generated-heading\">Generated Resume</h1>\n<div class=\"main-div\">\n <fieldset class=\"personal-div\">\n     <legend>Personal Information</legend>\n     <p class=\"generated-name\">Name: ".concat(name, "</p>\n     <p>Email: ").concat(email, "</p>\n     <p>Phone Number: ").concat(phone, "</p>\n </fieldset>\n <fieldset class=\"education-div\">\n     <legend>Education</legend>\n     <p class=\"generated-education\">").concat(education, "</p>\n </fieldset>\n <fieldset class=\"skills-div\">\n     <legend>Skills</legend>\n     <p class=\"generated-skills\">").concat(skills, "</p>\n </fieldset>\n <fieldset class=\"experience-div\">\n     <legend>Experience</legend>\n     <p class=\"generated-experience\">").concat(experience, "</p>\n </fieldset>\n</div>\n    ");
+    if (generatedResume) {
+        generatedResume.innerHTML = resumeHtml;
+    }
+});
---
+++ Milestone 3/script.ts
@@ -0,0 +1,43 @@
+
+const form = document.querySelector(".form-1") as HTMLFormElement;
+const generatedResume = document.querySelector(".generated-resume") as HTMLDivElement;
+
+form.addEventListener("submit" , (event : Event) => {
+    event.preventDefault();
+
+    const name =( document.querySelector("#name") as HTMLInputElement).value;
+    const email =( document.querySelector("#email") as HTMLInputElement).value;
+    const phone =( document.querySelector("#phone") as HTMLInputElement).value;
+    const education =( document.querySelector("#education") as HTMLInputElement).value;
+    const skills =( document.querySelector("#skills") as HTMLInputElement).value;
+    const experience =( document.querySelector("#experience") as HTMLInputElement).value;
+
+
+    const resumeHtml:string = `
+<h1 class="generated-heading">Generated Resume</h1>
+<div class="main-div">
+ <fieldset class="personal-div">
+     <legend>Personal Information</legend>
+     <p class="generated-name">Name: ${name}</p>
+     <p>Email: ${email}</p>
+     <p>Phone Number: ${phone}</p>
+ </fieldset>
+ <fieldset class="education-div">
+     <legend>Education</legend>
+     <p class="generated-education">${education}</p>
+ </fieldset>
+ <fieldset class="skills-div">
+     <legend>Skills</legend>
+     <p class="generated-skills">${skills}</p>
+ </fieldset>
+ <fieldset class="experience-div">
+     <legend>Experience</legend>
+     <p class="generated-experience">${experience}</p>
+ </fieldset>
+</div>
+    `
+    if(generatedResume){
+        generatedResume.innerHTML = resumeHtml;
+    }
+})
+
